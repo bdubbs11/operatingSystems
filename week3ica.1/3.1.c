9e9468c3263e43f846e210a5e6b3dbfc371054c9
Brandon Wilson <brandonwilson@MacBook-Pro-193.local>
2025-01-29 18:36:06 -0500

    refactor names to match with canvas

---
+++ week3ica.1/3.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{ 
+
+  int myNum[] = {25,50,75,100};
+
+  printf("%d \n", myNum[1]);  
+ 
+  return 0;
+
+
+}
--- week3ica.1/dereference3.2.c
+++ week3ica.1/dereference3.2.c
@@ -6,8 +6,7 @@ int main()
   int myNum[] = {25,50,75,100};
 
   for(int i = 0; i < sizeof(myNum) / sizeof(myNum[0]); i++){
-    printf("%d \n", *(myNum + i));
-    printf("%d \n", myNum[i])ca;  
+    printf("%d \n", myNum[i]);  
   }
  
   return 0;
